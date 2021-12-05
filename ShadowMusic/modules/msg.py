3f19029d434c343148618af2431d80b50d7b8508
CYBER WARRIOR <89768855+Cyber01warriors@users.noreply.github.com>
2021-12-05 07:21:35 -0800

    Update msg.py

--- ShadowMusic/modules/msg.py
+++ ShadowMusic/modules/msg.py
@@ -18,7 +18,7 @@ from ShadowMusic.config import ASSISTANT_NAME, PROJECT_NAME
 
 
 class Messages:
-    START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info."
+    START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info.\n\nDevlope By @Cyber01_Warriors"
     HELP_MSG = [
         ".",
         f"""
